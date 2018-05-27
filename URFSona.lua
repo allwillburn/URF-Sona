aef8a6f79d9d2f15c624fcd1bdafa37a7ea408cd
allwillburn <livefortoday470@gmail.com>
2018-05-27 08:33:12 -0400

    Update URFSona.lua

--- URFSona.lua
+++ URFSona.lua
@@ -1,25 +1,24 @@
-if GetObjectName(GetMyHero()) ~= "Sona" then return end
+if GetObjectName(GetMyHero()) ~= "Jax" then return end
 
-local SonaMenu = Menu("Sona", "Sona")
+local JaxMenu = Menu("Jax", "Jax")
 
-SonaMenu:SubMenu("Combo", "Combo")
+JaxMenu:SubMenu("Combo", "Combo")
 
-SonaMenu.Combo:Boolean("Q", "Use Q in combo", true)
-SonaMenu.Combo:Boolean("W", "Use W in combo", true)
-SonaMenu.Combo:Boolean("E", "Use E in combo", true)
-SonaMenu.Combo:Boolean("R", "Use R in combo", true)
+JaxMenu.Combo:Boolean("Q", "Use Q in combo", true)
+JaxMenu.Combo:Boolean("W", "Use W in combo", true)
+JaxMenu.Combo:Boolean("E", "Use E in combo", true)
+JaxMenu.Combo:Boolean("R", "Use R in combo", true)
 
-SonaMenu:SubMenu("Misc", "Misc")
-SonaMenu.Misc:Boolean("Level", "Auto level spells", true)
-SonaMenu.Misc:Boolean("Ghost", "Auto Ghost", true)
-SonaMenu.Misc:Boolean("QWE", "Auto QWE", true)
+JaxMenu:SubMenu("Misc", "Misc")
+JaxMenu.Misc:Boolean("Level", "Auto level spells", true)
+JaxMenu.Misc:Boolean("Ghost", "Auto Ghost", true)
 
 OnTick(function (myHero)
 
 	local target = GetCurrentTarget()
 
 	--AUTO LEVEL UP
-	if SonaMenu.Misc.Level:Value() then
+	if JaxMenu.Misc.Level:Value() then
 
 			spellorder = {_Q, _W, _E, _Q, _W, _R, _Q, _W, _W, _Q, _R, _Q, _W, _E, _E, _R, _E, _E}
 			if GetLevelPoints(myHero) > 0 then
@@ -31,43 +30,28 @@ OnTick(function (myHero)
 	--COMBO
 	if IOW:Mode() == "Combo" then
 
-		if SonaMenu.Combo.Q:Value() and Ready(_Q) and ValidTarget(target, 850) then
-			CastSpell(_Q)
+		if JaxMenu.Combo.Q:Value() and Ready(_Q) and ValidTarget(target, 700) then
+			CastTargetSpell(target, _Q)
 		end
 
-		if SonaMenu.Combo.W:Value() and Ready(_W) then
+		if JaxMenu.Combo.W:Value() and Ready(_W) and ValidTarget(target, 700) then
 			CastSpell(_W)
 		end
 
-		if SonaMenu.Combo.E:Value() and Ready(_E) then
+		if JaxMenu.Combo.E:Value() and Ready(_E) and ValidTarget(target, 700) then
 			CastSpell(_E)
 		end
 
-		if SonaMenu.Combo.R:Value() and Ready(_R) and ValidTarget(target, 900) then
-			CastSkillShot(_R, target)
+		if JaxMenu.Combo.R:Value() and Ready(_R) and ValidTarget(target, 700) then
+			CastSpell(_R)
 		end
 
 	end
 
-        --AUTO QWE
-        if SonaMenu.Misc.QWE:Value() then        
-
-                if Ready(_E) then
-		        CastSpell(_E)
-                end
-
-                if Ready(_Q) then
-	  	        CastSpell(_Q)
-                end
-
-	        if Ready(_W) then
-		        CastSpell(_W)
-	        end
-
-        end
+       
 
 	--AUTO IGNITE
-	if SonaMenu.Misc.Ghost:Value() then
+	if JaxMenu.Misc.Ghost:Value() then
 
 		if GetCastName(myHero, SUMMONER_1) == "SummonerHaste" and Ready(SUMMONER_1) then
 			CastSpell(SUMMONER_1)
